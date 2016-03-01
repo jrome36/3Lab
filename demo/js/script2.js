abd18f7a3781128b8bb3f20b5dd7ca23cdc32be2
jrome36 <88130@mail.lsue.edu>
2016-03-01 10:27:27 -0600

    Update script2.js

--- demo/js/script2.js
+++ demo/js/script2.js
@@ -1,8 +1,9 @@
+//zoom view of basemap
 var map = L.map('map').setView([ 30, -90], 4);
 var street = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',  {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
+//variable that inserts high temperature layers show opacity of 0.35. the next 3 variables are all completing the same task.
 var hightemps = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_dailymaxairtemp_offsets/MapServer/WMSServer", {
 	layers: '1',
 	format: 'image/png',
@@ -30,6 +31,7 @@ var baseLayers = {
 
 };
 
+//feature that allows title to show in right had top corner tool bar.
 var overlays= {
 "High Temp": hightemps,
 "Lightning": lightning,
